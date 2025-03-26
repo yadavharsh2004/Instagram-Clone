63c7ddab852f4ab9ef8a00995dbc945b2f51aeb2
yadavharsh2004 <harshy8013698@gmail.com>
2025-03-26 23:37:38 +0530

    Add Navigation for authPage and homePage

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,15 @@ import {
   useColorMode,
   useColorModeValue,
 } from "@/components/ui/color-mode"
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import AuthPage from './pages/AuthPage/AuthPage';
 import PageLayout from './Layouts/PageLayout/PageLayout';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
 import { Toaster, toaster } from "@/components/ui/toaster"
+import useAuthStore from './store/authStore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase/firebase';
 
 
 
@@ -36,13 +39,15 @@ function App() {
     // },
   };
 
+  const [authUser] = useAuthState(auth); //from firebase
+
   return (
     <Provider>
       <PageLayout>
 
         <Routes>
-          <Route path='/' element= {<HomePage />} />
-          <Route path='/auth' element= {<AuthPage />} />
+          <Route path='/' element= {authUser? <HomePage /> : <Navigate to="/auth" />} />
+          <Route path='/auth' element= {!authUser? <AuthPage /> : <Navigate to="/" /> } />
           <Route path='/:username' element= {<ProfilePage />} />
         </Routes>
 
