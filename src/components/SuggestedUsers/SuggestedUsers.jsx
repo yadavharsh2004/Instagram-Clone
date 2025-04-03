7adc664e9b6d39d8d53b3812dcb2c8a6b064bd66
yadavharsh2004 <harshy8013698@gmail.com>
2025-04-03 23:54:00 +0530

    Add Suggested User Functionality

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,36 +1,40 @@
 import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react";
 import SuggestedHeader from "./SuggestedHeader";
 import SuggestedUser from "./SuggestedUser";
+import useGetSuggestedUser from "../../hooks/useGetSuggestedUser";
 
 const SuggestedUsers = () => {
+  const { isLoading, suggestedUsers } = useGetSuggestedUser();
+
+  if (isLoading) return null;
+
   return (
     <>
       <VStack py={8} px={6} gap={4}>
         <SuggestedHeader />
 
-        <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
-          <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
-            Suggested for you
-          </Text>
-          <Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
-            See All
-          </Text>
-        </Flex>
+        {suggestedUsers.length !== 0 && (
+          <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+            <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+              Suggested for you
+            </Text>
+            <Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"} >
+              See All
+            </Text>
+          </Flex>
+        )}
 
-
-
-        <SuggestedUser name='Dan Abramov' followers={1392} avatar='https://bit.ly/dan-abramov' />
-        <SuggestedUser name='Ryan Florence' followers={567} avatar='https://bit.ly/ryan-florence' />
-        <SuggestedUser name='Christian Nwamba' followers={759} avatar='https://bit.ly/code-beast' />
-
-
+        {suggestedUsers.map((user) => (
+          <SuggestedUser user={user} key={user.id} />
+        ))}
 
         <Box fontSize={12} mt={5} color={"gray.500"} alignSelf={"start"}>
           © 2025 Built By{" "}
-
-          <Link href="https://github.com/yadavharsh2004"
+          <Link
+            href="https://github.com/yadavharsh2004"
             target="_blank"
-            color={"blue.500"} fontSize={14}
+            color={"blue.500"}
+            fontSize={14}
           >
             Harsh Yadav
           </Link>
