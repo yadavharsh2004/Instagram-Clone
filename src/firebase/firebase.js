45905dd34e2dcdfc6a40e7c1ebe029ab1c8ad828
yadavharsh2004 <harshy8013698@gmail.com>
2025-04-09 14:49:38 +0530

    Add vercel.json file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,7 +13,7 @@ import {getStorage} from "firebase/storage"
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,VITE_FIREBASE_PROJECT_ID,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
