2bdc88af003b9df280582e1432496cccd56c326b
yadavharsh2004 <harshy8013698@gmail.com>
2025-07-24 00:18:43 +0530

    Follow users in mobile device fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,12 +7,12 @@ const HomePage = () => {
   return (
     <>
       <Container maxW={"container.lg"}>
-        <Flex gap={20}>
+        <Flex gap={20} direction={{ base: "column", lg: "row" }}>
           <Box maxW={"container.lg"} flex={2} py={10}  >
             <FeedPosts />
           </Box>
 
-          <Box flex={3} mr={20} display={{ base: "none", lg: "block" }}
+          <Box flex={3} mr={20} display="block"
             maxW={"300px"} 
           >
             <SuggestedUsers />
